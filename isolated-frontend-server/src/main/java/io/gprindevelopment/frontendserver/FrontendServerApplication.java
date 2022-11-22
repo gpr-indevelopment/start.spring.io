91ce24c753cf1d41bf6b22b9ec45b618063e79ef
Gabriel Robaina <gpr.indevelopment@gmail.com>
2022-11-21 21:57:15 -0300

    Added function module

--- isolated-frontend-server/src/main/java/io/gprindevelopment/frontendserver/FrontendServerApplication.java
+++ isolated-frontend-server/src/main/java/io/gprindevelopment/frontendserver/FrontendServerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FrontendServerApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(FrontendServerApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(FrontendServerApplication.class, args);
+	}
 
 }
